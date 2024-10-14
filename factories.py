34c2d394173b4082a212b83f3dbd3d6f6281f4fa
la-Alisa <nirononly@mail.ru>
2024-10-14 12:54:44 +0300

    refactor: update for work with attributes

--- factories.py
+++ factories.py
@@ -1,34 +1,35 @@
 from abc import ABC, abstractmethod
 
+from attributes import Attributes, GrassAttributes, HerbivoreAttributes, PredatorAttributes, RockAttributes, TreeAttributes
 from entities import Entity, Grass, Herbivore, Tree, Rock, Predator
 
 
 class Factory(ABC):
     @abstractmethod
-    def create(self) -> Entity:
+    def create(self, attributes: Attributes) -> Entity:
         pass
 
 
 class GrassFactory(Factory):
-    def create(self) -> Grass:
-        return Grass()
+    def create(self, attributes: GrassAttributes) -> Grass:
+        return Grass(hp=attributes.hp)
 
 
 class HerbivoreFactory(Factory):
-    def create(self) -> Herbivore:
-        return Herbivore()
+    def create(self, attributes: HerbivoreAttributes) -> Herbivore:
+        return Herbivore(hp=attributes.hp, action_points=attributes.action_points, attack=attributes.attack)
 
 
 class TreeFactory(Factory):
-    def create(self) -> Tree:
-        return Tree()
+    def create(self, attributes: TreeAttributes) -> Tree:
+        return Tree(hp=attributes.hp)
 
 
 class RockFactory(Factory):
-    def create(self) -> Rock:
-        return Rock()
+    def create(self, attributes: RockAttributes) -> Rock:
+        return Rock(hp=attributes.hp)
 
 
 class PredatorFactory(Factory):
-    def create(self) -> Predator:
-        return Predator()
+    def create(self, attributes: PredatorAttributes) -> Predator:
+        return Predator(hp=attributes.hp, action_points=attributes.action_points, attack=attributes.attack)
