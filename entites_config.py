c5eb09d18ad952863594c5ec93d194dd14cb3db9
la-Alisa <nirononly@mail.ru>
2024-12-29 16:13:23 +0300

    feat: add entity, game configs tuples

---
+++ entites_config.py
@@ -0,0 +1,22 @@
+from typing import NamedTuple, Type
+
+from attributes import Attributes, GrassAttributes, HerbivoreAttributes, PredatorAttributes, RockAttributes, TreeAttributes
+from entities import Entity, Grass, Herbivore, Predator, Rock, Tree
+from factories import Factory, GrassFactory, HerbivoreFactory, PredatorFactory, RockFactory, TreeFactory
+
+
+class EntityConfig(NamedTuple):
+    spawn_coeff: int
+    respawn_coeff: int
+    factory: Type[Factory]
+    attributes: Type[Attributes]
+
+
+def get_entities_config() -> dict[Entity, EntityConfig]:
+    return {
+        Herbivore: EntityConfig(spawn_coeff=0.07, respawn_coeff=0.07, factory=HerbivoreFactory, attributes=HerbivoreAttributes),
+        Predator: EntityConfig(spawn_coeff=0.05, respawn_coeff=0.05, factory=PredatorFactory, attributes=PredatorAttributes),
+        Grass: EntityConfig(spawn_coeff=0.09, respawn_coeff=0.09, factory=GrassFactory, attributes=GrassAttributes),
+        Tree: EntityConfig(spawn_coeff=0.03, respawn_coeff=0.009, factory=TreeFactory, attributes=TreeAttributes),
+        Rock: EntityConfig(spawn_coeff=0.01, respawn_coeff=0.009, factory=RockFactory, attributes=RockAttributes),
+    }
