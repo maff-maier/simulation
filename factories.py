28a8395a0c0c0604e19c2cde0c54db027c8740a0
la-Alisa <nirononly@mail.ru>
2024-12-29 16:11:50 +0300

    refactor: change object method to class method

--- factories.py
+++ factories.py
@@ -6,32 +6,38 @@ from entities import Entity, Grass, Herbivore, Predator, Rock, Tree
 
 
 class Factory(ABC):
+    @classmethod
     @abstractmethod
-    def create(self, attributes: Type[Attributes]) -> Entity:
+    def create(cls, attributes: Type[Attributes]) -> Entity:
         pass
 
 
 class HerbivoreFactory(Factory):
-    def create(self, attributes: Type[Attributes]) -> Herbivore:
+    @classmethod
+    def create(cls, attributes: Type[Attributes]) -> Herbivore:
         return Herbivore(hp=attributes.hp, action_points=attributes.action_points, damage=attributes.damage)
 
 
 class PredatorFactory(Factory):
-    def create(self, attributes: Type[Attributes]) -> Predator:
+    @classmethod
+    def create(cls, attributes: Type[Attributes]) -> Predator:
         return Predator(hp=attributes.hp, action_points=attributes.action_points, damage=attributes.damage)
 
 
 class GrassFactory(Factory):
-    def create(self, attributes: Type[Attributes]) -> Grass:
+    @classmethod
+    def create(cls, attributes: Type[Attributes]) -> Grass:
         return Grass(hp=attributes.hp)
 
 
 class RockFactory(Factory):
-    def create(self, attributes: Type[Attributes]) -> Rock:
+    @classmethod
+    def create(cls, attributes: Type[Attributes]) -> Rock:
         return Rock(hp=attributes.hp)
 
 
 class TreeFactory(Factory):
-    def create(self, attributes: Type[Attributes]) -> Tree:
+    @classmethod
+    def create(cls, attributes: Type[Attributes]) -> Tree:
         return Tree(hp=attributes.hp)
 
