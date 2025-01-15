7efd15f1bdb1d159f0f2ab43189f34a3d4e05d05
la-Alisa <nirononly@mail.ru>
2025-01-15 22:48:37 +0300

    feat: create menu manager

---
+++ menu.py
@@ -0,0 +1,9 @@
+class Menu:
+    def get_start_menu(self) -> str:
+        return f'1. Start\n2. End\n'
+    
+    def get_pause_menu(self) -> str:
+        return f'1. Resume\n2. Reset\n3. End\n'
+    
+    def get_continuing_menu(self) -> str:
+        return f'1. Pause\n2. Reset\n3. End\n'
